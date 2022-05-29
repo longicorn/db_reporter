fdb7883177e23021d52ef441214c1d556a60377b
longicorn <longicorn.c@gmail.com>
2022-05-29 20:02:22 +0900

    First commit

---
+++ lib/db_reporter/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DbReporter
+  VERSION = "0.1.0"
+end
---
+++ spec/db_reporter_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe DbReporter do
+  it "has a version number" do
+    expect(DbReporter::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
